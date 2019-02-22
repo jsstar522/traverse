aa386b8727b3c4ada3b741e3d4d2b40266bd2923
JongSuk <jsstar522@gmail.com>
2019-02-22 20:34:46 +0900

    Fix router that writing data on DB is not working

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,22 +5,37 @@ const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { User } = require('../models/user');
 
 const router = express.Router();
+
+// Account signup router
 router.post('/register', isNotLoggedIn, async (req, res, next) => {
   const {email, password} = req.body;
   try {
+    //찾지 못했을 시 빈 배열로 출력
     const exUser = await User.find({ email: email });
-    if (exUser) {
+    if (exUser.length != 0) {
       req.flash('registerError', '이미 가입된 이메일입니다.');
+      console.log('이미가입된 이메일');
+      console.log(exUser)
       return res.redirect('/register');
     }
-    const rebody = Object.keys(req.body).map(async (key) => {    
+    // const rebody = Object.keys(req.body).map(async (key) => {    
+    //   if (key === 'password')
+    //     return { key: await bcrypt.hash(req.body[password], 12)}
+    //   else return { key: req.body[key] };
+    // });
+
+    // Create Object to wirte on database
+    let rebody = new Object();
+
+    await Object.keys(req.body).map((key)=>{
       if (key === 'password')
-        return { key: await bcrypt.hash(req.body[password], 12)}
-      else return { key: req.body[key] };
+        rebody[key] = bcrypt.hash(req.body[password], 12);      
+      rebody[key] = req.body[key];    
     });
+
     let user = new User(rebody);
     user = await user.save();
-  
+
     return res.json(user);
   } catch (error) {
     console.error(error);
@@ -28,6 +43,7 @@ router.post('/register', isNotLoggedIn, async (req, res, next) => {
   }
 });
 
+// Account signin router
 router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
     if (authError) {
@@ -48,12 +64,14 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
   })(req, res, next);
 });
 
+// Account logout router
 router.get('/logout', isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
   res.redirect('/');
 })
 
+// AUTH
 router.get('/kakao', passport.authenticate('kakao'));
 router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
 
